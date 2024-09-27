fa2d3bdd0dcd068eb1b30ee65fe2ab4ebc1289fc
SunoopDogg <aswoo55555@gmail.com>
2024-09-27 15:11:55 +0900

    chore: Add dynamic px scale

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,27 @@
 import type { Config } from 'tailwindcss';
 
+const px0_100 = Object.fromEntries(
+  Array.from(Array(101)).map((_, i) => [i, `${i}px`]),
+);
+
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        ...px0_100,
+      },
+      spacing: {
+        ...px0_100,
+      },
+      borderRadius: {
+        ...px0_100,
+      },
+      borderWidth: {
+        ...px0_100,
+      },
+      padding: {
+        ...px0_100,
       },
     },
   },
