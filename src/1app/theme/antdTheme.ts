5aec054d54815f1fda18ab62541410e087d1b6b1
SunoopDpgg <aswoo55555@gmail.com>
2025-03-26 15:07:22 +0000

    feat: Add Ant Design theme configuration with Pretendard font

---
+++ src/1app/theme/antdTheme.ts
@@ -0,0 +1,7 @@
+import type { ThemeConfig } from 'antd';
+
+export const antdTheme: ThemeConfig = {
+  token: {
+    fontFamily: 'var(--font-pretendard)',
+  },
+};
---
+++ src/1app/theme/index.ts
@@ -0,0 +1,1 @@
+export { antdTheme } from './antdTheme';
