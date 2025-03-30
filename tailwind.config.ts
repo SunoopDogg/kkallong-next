3a78568f17ca26bc8727a75c64826755368234aa
SunoopDpgg <aswoo55555@gmail.com>
2025-03-30 08:15:15 +0000

    chore: Simplify configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,7 @@
 import type { Config } from 'tailwindcss';
 
-const px0_100 = Object.fromEntries(
-  Array.from(Array(101)).map((_, i) => [i, `${i}px`]),
-);
-
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      fontSize: {
-        ...px0_100,
-      },
-      spacing: {
-        ...px0_100,
-      },
-      borderRadius: {
-        ...px0_100,
-      },
-      borderWidth: {
-        ...px0_100,
-      },
-      padding: {
-        ...px0_100,
-      },
-    },
-  },
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   plugins: [],
 };
 export default config;
