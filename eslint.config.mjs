ba03e57c7ee37168396acb4d5d11fb54a70bf21a
SunoopDogg <aswoo55555@gmail.com>
2026-02-11 18:32:04 +0000

    chore: Update ESLint and Prettier configurations for improved import sorting and organization

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,58 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import perfectionist from 'eslint-plugin-perfectionist';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  {
+    plugins: { perfectionist },
+    rules: {
+      'import/order': 'off',
+      'sort-imports': 'off',
+
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'natural',
+          order: 'asc',
+          ignoreCase: true,
+          internalPattern: ['^@/.+'],
+          newlinesBetween: 1,
+          groups: [
+            'type-import',
+            'react',
+            'next',
+            'value-builtin',
+            'value-external',
+            'value-internal',
+            ['value-parent', 'value-sibling', 'value-index'],
+            'side-effect',
+            'side-effect-style',
+            'style',
+            'unknown',
+          ],
+          customGroups: [
+            {
+              groupName: 'react',
+              modifiers: ['value'],
+              selector: 'import',
+              elementNamePattern: ['^react$', '^react-dom'],
+            },
+            {
+              groupName: 'next',
+              modifiers: ['value'],
+              selector: 'import',
+              elementNamePattern: ['^next$', '^next/'],
+            },
+          ],
+          environment: 'node',
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
